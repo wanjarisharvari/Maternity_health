7535b682180d3a58ad8f471c0ceca8ef58aa39ad
Sekhar-7 <143331057+Sekhar-7@users.noreply.github.com>
2025-02-13 16:24:57 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,10 @@
-echo "BUILD START"
-python3 -m pip install -r requirements.txt
+#echo "BUILD START"
+#python3 -m pip install -r requirements.txt
+#python3 manage.py collectstatic --noinput --clear
+#echo "BUILD END"
+
+#!/bin/bash
+pip install --upgrade pip  # Upgrade pip
+pip install --no-cache-dir numpy  # Force install NumPy
+pip install --no-cache-dir -r requirements.txt  # Install all dependencies again
 python3 manage.py collectstatic --noinput --clear
-echo "BUILD END"
