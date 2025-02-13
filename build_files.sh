eef15329f91804de79c53fefe69311073247c6c3
Sekhar-7 <143331057+Sekhar-7@users.noreply.github.com>
2025-02-13 17:19:33 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@
 echo "BUILD START"
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput 
 echo "BUILD END"
 
 
