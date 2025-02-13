4cb0a8f0277ed55cfc6ea105cb05127fefcaa8ad
Sekhar-7 <143331057+Sekhar-7@users.noreply.github.com>
2025-02-13 16:49:22 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,20 +1,9 @@
-#echo "BUILD START"
-#python3 -m pip install -r requirements.txt
-#python3 manage.py collectstatic --noinput --clear
-#echo "BUILD END"
-
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "BUILD START"
-
-# Upgrade pip
-python3 -m pip install --upgrade pip --break-system-packages
-
-# Install dependencies without cache and suppress root warning
-python3 -m pip install -r requirements.txt --no-cache-dir --break-system-packages
-
-# Collect static files (if using Django)
+python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
-
 echo "BUILD END"
+
+
 
